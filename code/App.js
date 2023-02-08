31879f448684ca4146ff6aa30624de532e58b272
Gurdev Singh <106698199+whogurdevil@users.noreply.github.com>
2023-02-08 15:11:23 +0530

    component viewtask completed

--- code/App.js
+++ code/App.js
@@ -3,6 +3,10 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "./MyComp/Home";
 import AddTask from "./MyComp/AddTask";
+import ViewTask from "./MyComp/ViewTask";
+import { getStyle } from "react-native-svg/lib/typescript/xml";
+const colors = ['#e4def2', '#e2ddd8','#eef8ef','#2d414e','#E0DFE3']
+
 
 function App() {
   const Stack = createNativeStackNavigator();
@@ -17,7 +21,14 @@ function App() {
           name="AddTask"
           component={AddTask}
           />
-           
+          <Stack.Screen
+          name="ViewTask"
+          component={ViewTask}
+          options={{
+            title: 'All Tasks',
+          }}
+
+          />
         </Stack.Navigator>
       </NavigationContainer>
    );
--- code/MyComp/AddTask.js
+++ code/MyComp/AddTask.js
@@ -1,29 +1,82 @@
-import { DatePickerIOSBase, Dimensions, StyleSheet, Text, TextInput, View, modal } from "react-native"
+import React from 'react';
+import {  View, Text, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import DatePicker from 'react-native-date-picker'
-import React from "react"
+const styles = StyleSheet.create({
+  container: {
+    padding: 20
+  },
+  todoContainer: {
+    marginVertical: 10,
+    padding: 10,
+    backgroundColor: '#f0f0f0',
+    borderRadius: 5
+  },
+  todoTitle: {
+    fontWeight: 'bold'
+  },
+  addButton: {
+    marginVertical: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: '#3399ff',
+    borderRadius: 5
+  },
+  addButtonText: {
+    color: '#ffffff',
+    fontWeight: 'bold'
+  }
+});
 
-const {scale} = Dimensions.get("window")
-const colors = ['#e4def2', '#e2ddd8','#eef8ef','#2d414e']
-const AddTask = () =>{
-    return(
-        <View>
-            <TextInput 
-            placeholder="Title"
-            allowFontScaling={false}
-            style={{ margin:scale*5, backgroundColor:'#E0DFE3', fontSize: scale*6, padding:scale*5, paddingLeft:scale*7, borderRadius:scale*5, borderColor:colors[3]}}
-            ></TextInput>
+class AddTask extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: []
+    };
+  }
 
-            <TextInput
-            placeholder="Description"
-            multiline={true}
-            allowFontScaling={false}
-            style={{ margin:scale*5, backgroundColor:'#E0DFE3', fontSize: scale*6, padding:scale*5, paddingLeft:scale*7, borderRadius:scale*5, borderColor:colors[3]}}
-            ></TextInput>
+  componentDidMount() {
+    this.retrieveData();
+  }
 
-        </View>
-    )
+  addTodo = async () => {
+    const newTodo = { title: 'New Todo', description: 'Description' };
+    try {
+      const todos = [...this.state.todos, newTodo];
+      this.setState({ todos });
+      await AsyncStorage.setItem('todos', JSON.stringify(todos));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  retrieveData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('todos');
+      if (value !== null) {
+        this.setState({ todos: JSON.parse(value) });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  render() {
+    return (
+      <ScrollView style={styles.container}>
+        <TouchableOpacity onPress={this.addTodo} style={styles.addButton}>
+          <Text style={styles.addButtonText}>Add Todo</Text>
+        </TouchableOpacity>
+        {this.state.todos.map((todo, index) => (
+          <View key={index} style={styles.todoContainer}>
+            <Text style={styles.todoTitle}>{todo.title}</Text>
+            <Text>{todo.description}</Text>
+          </View>
+        ))}
+      </ScrollView>
+    );
+  }
 }
+
 export default AddTask
-
-
