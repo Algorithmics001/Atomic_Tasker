5e43035602df0a93e1160b28dc38f22ffa96a2f6
Raghav Jit <98019979+RaghavJit@users.noreply.github.com>
2023-02-09 15:18:30 +0530

    added brain and react-native-fs

---
+++ code/brain/UtilityFuncs.js
@@ -0,0 +1,28 @@
+// contains periferal functions for logic.js
+
+const fs = require('react-native-fs')
+const path = './Avalible_ID.json'
+
+exports.GetAvalibleID = (callback) => {
+    fs.readFile(path, 'utf-8', (err, jsonString) => {
+        if(err){
+            console.log(err)
+        }
+        else{
+            const data = JSON.parse(jsonString);
+            if (parseInt(data.AvalibleID.length) < 0){
+                return NULL
+            }
+            else {
+                var result = parseInt(data.AvalibleID[0]);
+                data.AvalibleID.splice(0, 1);
+
+                jsonString = JSON.stringify(data)    
+                fs.writeFileSync(path, jsonString, 'utf-8');
+
+                callback(result, null)
+            }
+        }
+    })
+}
+
